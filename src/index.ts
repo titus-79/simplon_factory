538db22046b0cee15227c9a2989ab5cc9172d736
titus-79 <titus@ik.me>
2026-05-07 10:28:35 +0200

    feat: productId class

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,14 @@
 import {Customer} from "./class/customer.js";
+import {ProductId} from "./class/productId.js";
 
 const message: string = "Hello World!";
 console.log(message);
 
 const person1 = new Customer(1,"Alice", "alice@mail.com");
 console.log(person1.displayInfo());
+
+class Derived extends ProductId {
+};
+
+const d = new Derived(12, "Nike", 350, 159.99);
+console.log(d.displayDetails());
