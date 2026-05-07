cf5510e5bd8a4440c65f8fc96a74b7d01e58c655
titus-79 <58149956+titus-79@users.noreply.github.com>
2026-05-07 10:29:18 +0200

    feat: productId class (#3)

---
+++ src/class/productId.ts
@@ -0,0 +1,16 @@
+export abstract class ProductId {
+    productId: number;
+    name: string;
+    weight: number;
+    price: number;
+
+    constructor(productId: number, name: string, weight: number, price: number) {
+        this.productId = productId;
+        this.name = name;
+        this.weight = weight;
+        this.price = price;
+    }
+
+    displayDetails(): string {
+        return `Product ID: ${this.productId}, Name: ${this.name}, Weight: ${this.weight}, Price: ${this.price} €`}
+}
